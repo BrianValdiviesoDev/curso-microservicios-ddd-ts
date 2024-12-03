0a280da3446381e177c8e4c629fb8cdf46112139
Brian Valdivieso <brian.valdivieso.dev@gmail.com>
2024-12-03 13:26:10 +0100

    Add family scafolding

---
+++ family/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import expressConfig from './framework/express';
+import { ErrorHandler } from './framework/middlewares/errorHandler';
+
+const app = express();
+expressConfig(app);
+
+app.use(express.json());
+
+// Ruta de checking
+app.get('/monit/health', (req: Request, res: Response) => {
+	res.send('Healthy');
+});
+
+
+app.use(ErrorHandler.handle);
+
+export { app };
